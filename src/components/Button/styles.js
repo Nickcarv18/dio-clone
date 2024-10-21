6732cabf834fc8ae5085af73c83503aa9b80574c
Nicoli Carvalho <nicoli.pereira@e-construmarket.com.br>
2024-10-21 20:51:36 -0300

    Criação componnete de button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    background: #565656;
+    border-radius: 22px;
+    position: relative;
+
+    color: #fff;
+    padding: 2px 12px;
+    min-width: 120px;
+    width: 100%;
+    border: none;
+    cursor: pointer;
+    margin-left:16px;
+
+    ${({variant}) => variant !== "primary" && `
+        min-width: 167px;
+        height: 33px;
+        background: #E41050;
+        margin-left: 0px;
+        margin-top: 16px;
+
+        &:hover{
+            opacity: 0.6;
+        }
+
+        &::after{
+            content: '';
+            position: absolute;
+            border: 1px solid #E41050;
+            top: -5px;
+            left: -6px;
+
+            width: calc(100% + 10px);
+            height: calc(100% + 10px);
+            border-radius:22px
+        }
+    `} 
+`
